0f23a712cc33f5a1bb80f07300540bed18c4fde5
Theodore Keloglou <zf@sirodoht.com>
2022-04-18 13:14:35 +0100

    add main go file and index template

---
+++ main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"html/template"
+	"net/http"
+	"fmt"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+)
+
+func main() {
+	// database
+	db, err := sqlx.Connect("postgres", "user=luhmn dbname=luhmn sslmode=disable")
+    if err != nil {
+        log.Fatalln(err)
+    }
+
+	// router
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+	r.Use(middleware.RedirectSlashes)
+
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		t, err := template.ParseFiles("templates/index.html")
+		if err != nil {
+			panic(err)
+		}
+		t.Execute(w, nil)
+	})
+
+	fmt.Println("Listening on http://127.0.0.1:8000/")
+	http.ListenAndServe(":8000", r)
+}
